635d1491acfc685c49a2e1365ef44990b75924d6
Rishabh Shukla <127231574+rishabhshuklayt@users.noreply.github.com>
2024-07-17 13:27:33 +0530

    adding again with all problems resolved

---
+++ model/leavedata.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+
+const requestSchema = new mongoose.Schema({
+    fname:String,
+    rollno:Number,
+    leaveType:String,
+    email:String,
+    startDate:Date,
+    endDate:Date,
+    reason:String,
+    halfDay:String,
+    status: String
+})
+
+
+
+const Request = mongoose.model('Request', requestSchema );
+
+
+module.exports = Request
